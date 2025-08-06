789483e2d74c6bac8d870a05120cc30698b8fa44
yafet <yafetesfaye11@gmail.com>
2025-08-07 00:11:35 +0300

    feat: add sign up page

---
+++ lib/features/auth/presentation/pages/signup_page.dart
@@ -0,0 +1,270 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  final formkey = GlobalKey<FormState>();
+  final _emailController = TextEditingController();
+  final _passwordController = TextEditingController();
+  final _nameController = TextEditingController();
+  final _confirmPasswordController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        leading: Padding(
+          padding: const EdgeInsets.only(left: 40),
+          child: Icon(Icons.arrow_back_ios_new),
+        ),
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 20),
+            child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 5, vertical: 2),
+              decoration: BoxDecoration(
+                border: Border.all(
+                  color: const Color.fromARGB(255, 93, 78, 252),
+                ),
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Text(
+                'ECOM',
+                style: GoogleFonts.aBeeZee(
+                  color: const Color.fromARGB(255, 93, 78, 252),
+                  fontSize: 20,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40, vertical: 70),
+
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Center(
+                child: Text(
+                  'Create your account',
+                  style: GoogleFonts.poppins(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+              SizedBox(height: 30),
+
+              Form(
+                key: formkey,
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Name',
+                      style: GoogleFonts.poppins(color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'name is required';
+                        }
+                        return null;
+                      },
+
+                      style: TextStyle(color: Colors.black),
+                      controller: _nameController,
+                      decoration: InputDecoration(
+                        hintText: 'ex: john smith',
+                        hintStyle: GoogleFonts.poppins(color: Colors.grey),
+                        fillColor: const Color.fromARGB(255, 241, 239, 239),
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Email',
+                      style: GoogleFonts.poppins(color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'email is required';
+                        }
+                        return null;
+                      },
+                      style: TextStyle(color: Colors.black),
+                      controller: _emailController,
+                      decoration: InputDecoration(
+                        hintText: 'ex: john@gmail.com',
+                        hintStyle: GoogleFonts.poppins(color: Colors.grey),
+                        fillColor: const Color.fromARGB(255, 241, 239, 239),
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Password',
+
+                      style: GoogleFonts.poppins(color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'password is required';
+                        }
+                        return null;
+                      },
+                      style: TextStyle(color: Colors.black),
+                      controller: _passwordController,
+                      decoration: InputDecoration(
+                        hintText: '*********',
+                        hintStyle: GoogleFonts.poppins(color: Colors.grey),
+                        fillColor: const Color.fromARGB(255, 241, 239, 239),
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+                    SizedBox(height: 10),
+                    Text(
+                      'Confirm Password',
+
+                      style: GoogleFonts.poppins(color: Colors.grey),
+                    ),
+                    SizedBox(height: 10),
+                    TextFormField(
+                      validator: (value) {
+                        if (value == null || value.isEmpty) {
+                          return 'email is required';
+                        }
+
+                        return null;
+                      },
+                      style: TextStyle(color: Colors.black),
+                      controller: _confirmPasswordController,
+                      decoration: InputDecoration(
+                        hintText: '*********',
+                        hintStyle: GoogleFonts.poppins(color: Colors.grey),
+                        fillColor: const Color.fromARGB(255, 241, 239, 239),
+                        filled: true,
+                        contentPadding: EdgeInsets.symmetric(
+                          horizontal: 15,
+                          vertical: 10,
+                        ),
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10),
+                          borderSide: BorderSide.none,
+                        ),
+                      ),
+                    ),
+
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        Checkbox(
+                          value: true,
+                          onChanged: (value) {
+                            setState(() {});
+                          },
+                          activeColor: const Color.fromARGB(255, 93, 78, 252),
+                        ),
+                        Text.rich(
+                          TextSpan(
+                            children: [
+                              TextSpan(text: 'I understood the '),
+                              TextSpan(text: 'term & policy.'),
+                            ],
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    SizedBox(height: 40),
+                    SizedBox(
+                      width: double.infinity,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(
+                            255,
+                            93,
+                            78,
+                            252,
+                          ),
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadiusGeometry.circular(10),
+                          ),
+                        ),
+                        onPressed: () {},
+                        child: Text(
+                          'SIGN Up',
+                          style: GoogleFonts.poppins(color: Colors.white),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: Text.rich(
+                  TextSpan(
+                    children: [
+                      TextSpan(
+                        text: "Have an account ?",
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      TextSpan(
+                        text: 'SIGN IN',
+                        style: TextStyle(
+                          color: const Color.fromARGB(255, 93, 78, 252),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:g6_assessment/features/auth/presentation/pages/login_page.dart';
+import 'package:g6_assessment/features/auth/presentation/pages/signup_page.dart';
 
 void main() {
-  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: LoginPage()));
+  runApp(MaterialApp(debugShowCheckedModeBanner: false, home: SignupPage()));
 }
