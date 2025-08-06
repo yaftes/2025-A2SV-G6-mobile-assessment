862378635b37a0752bc176d09ddf47b5c2a807aa
yafet <yafetesfaye11@gmail.com>
2025-08-06 15:26:47 +0300

    feat: add auth repository

---
+++ lib/features/auth/domain/repositories/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:g6_assessment/core/error/failures.dart';
+import 'package:g6_assessment/features/auth/domain/entities/user.dart';
+
+abstract class AuthRepository {
+  Future<Either<Failure, User>> signUp();
+  Future<Either<Failure, User>> login();
+  Future<Either<Failure, User>> logout();
+}
